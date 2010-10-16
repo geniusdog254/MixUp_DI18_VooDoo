675df3758fcad1c6290922ed8f3806ff85c3ca63
Jesse C <geniusdog254@gmail.com>
2010-10-16 16:52:17 -0500

    Restore to default light sensor values, since changing them at all breaks everything. Stupid Samsung coding.

--- include/linux/compile.h
+++
@@ -1,8 +0,0 @@
-/* This file is auto generated, version 7 */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#7 Tue Oct 12 19:33:26 CDT 2010"
-#define LINUX_COMPILE_TIME "19:33:26"
-#define LINUX_COMPILE_BY "jcureton"
-#define LINUX_COMPILE_HOST "jcureton-laptop"
-#define LINUX_COMPILE_DOMAIN ""
-#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
