2c49f020135b77e6b215ab4fc60b06c946cfe822
Devin T. Andelin <these7enthprophet@gmail.com>
2010-10-25 10:12:35 -0600

    Call me the garbage man becuase I am takng out the trash... post-compiled...

--- include/linux/compile.h
+++
@@ -1,8 +0,0 @@
-/* This file is auto generated, version 1 */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 Mon Oct 25 01:50:40 MDT 2010"
-#define LINUX_COMPILE_TIME "01:50:40"
-#define LINUX_COMPILE_BY "devinxtreme"
-#define LINUX_COMPILE_HOST "EpicLaptop"
-#define LINUX_COMPILE_DOMAIN ""
-#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
